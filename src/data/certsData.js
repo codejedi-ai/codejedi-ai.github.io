9b9706bbe6cc9ead9095440617dae478dd329d30
Darcy Liu <darcy.ldx@gmail.com>
2025-02-10 12:05:58 -0500

    made update to clean up the data sources

---
+++ src/data/certsData.js
@@ -0,0 +1,25 @@
+export const skillsData = [
+    {
+        img: '/img/aws-practitioner.png',
+        alt: 'AWS-CloudPractitioner-2021',
+        title: 'AWS Certified Practitioner',
+        date: '2 January 2021'
+    },
+    {
+        img: '/img/aws-developer.png',
+        alt: 'AWS-Developer', 
+        title: 'AWS Certified Developer',
+        date: '29 August 2021'
+    },
+    {
+        img: '/img/aws-devops-prof.png',
+        alt: 'AWS-SysOps',
+        title: 'AWS Certified DevOps Engineer - Professional',
+        date: '23 August 2024'
+    }
+];
+
+export const skillsQuote = {
+    text: "The greatest scientific discovery was the discovery of ignorance.",
+    author: "Yuval Noah Harari, Homo Deus: A History of Tomorrow"
+};
