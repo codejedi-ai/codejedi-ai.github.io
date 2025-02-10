57f5ca638b77f84833f9138be482003398e76008
Darcy Liu <darcy.ldx@gmail.com>
2025-02-10 13:50:38 -0500

    revert

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,5 +1,30 @@
-import { skillsQuote, skillsData } from '../data/certsData.js';
-export function generateSkillsSection(skillsQuote, skillsData) {
+export const skillsData = [
+    {
+        img: '/img/aws-practitioner.png',
+        alt: 'AWS-CloudPractitioner-2021',
+        title: 'AWS Certified Practitioner',
+        date: '2 January 2021'
+    },
+    {
+        img: '/img/aws-developer.png',
+        alt: 'AWS-Developer', 
+        title: 'AWS Certified Developer',
+        date: '29 August 2021'
+    },
+    {
+        img: '/img/aws-devops-prof.png',
+        alt: 'AWS-SysOps',
+        title: 'AWS Certified DevOps Engineer - Professional',
+        date: '23 August 2024'
+    }
+];
+
+export const skillsQuote = {
+    text: "The greatest scientific discovery was the discovery of ignorance.",
+    author: "Yuval Noah Harari, Homo Deus: A History of Tomorrow"
+};
+
+export function generateSkillsSection() {
     let section = document.getElementById('skills');
     
     // Create section if it doesn't exist
@@ -82,7 +107,7 @@ export function setSkillsBackground() {
 }
 
 function initializeAll() {
-    generateSkillsSection(skillsQuote, skillsData);
+    generateSkillsSection();
     setSkillsBackground();
 }
 
